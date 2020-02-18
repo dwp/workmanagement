52b94c3407f89eab468a180bd9190ffa4ecd3fd8
Phil Swan <mail@philswan.co.uk>
2020-02-18 16:32:32 +0000

    First stab at v5 with dynamic stuff (wkind of wroking from ESA)

--- app/routes/v1.js
+++ app/routes/v1.js
@@ -14,8 +14,53 @@ module.exports = function (app) {
     next();
   });
 
-  // VERSION 1 STUFF HERE
+  // Code from Steven for dealing with variables on list page
 
-  
+  app.get('/v5/confirmation-message', (req, res, next) => {
+
+    if (!req.session.sectionStatus){
+      console.log('no session');
+      req.session.sectionStatus = {
+        // cwyn: 'complete',
+        yourhealth: undefined,
+        paidwork: undefined,
+        ssp: undefined,
+        voluntarywork: undefined,
+        pension: undefined,
+        insurance: undefined,
+        yourdetails: undefined,
+        // submitted: undefined,
+      }
+    }
+
+    if (req.query.yourhealth) {
+      req.session.sectionStatus.yourhealth = req.query.yourhealth
+    };
+    if (req.query.paidwork) {
+      req.session.sectionStatus.paidwork = req.query.paidwork
+    };
+    if (req.query.ssp) {
+      req.session.sectionStatus.ssp = req.query.ssp
+    };
+    if (req.query.voluntarywork) {
+      req.session.sectionStatus.voluntarywork = req.query.voluntarywork
+    };
+    if (req.query.pension) {
+      req.session.sectionStatus.pension = req.query.pension
+    };
+    if (req.query.insurance) {
+      req.session.sectionStatus.insurance = req.query.insurance
+    };
+    if (req.query.yourdetails) {
+      req.session.sectionStatus.yourdetails = req.query.yourdetails
+    };
+    if (req.query.submitted) {
+      req.session.sectionStatus.submitted = req.query.submitted
+    };
+
+    res.render('v5/confirmation-message.html', {sectionStatus: req.session.sectionStatus});
+    
+  });
+
 
 }
