05751a068b244a92a425821d9164d9cd77438e01
Phil Swan <mail@philswan.co.uk>
2020-02-21 16:27:28 +0000

    Starting to flesh out

--- app/data/v6-data.js
+++ app/data/v6-data.js
@@ -2,24 +2,74 @@ module.exports = function(req, res, next) {
   
   res.locals.cases = [
     {
-      name: "Fred",
+      name: "Fred Flintstone",
       benefit: "UC",
-      nino: "1234"
+      nino: "XX XX XX XX X",
+      dob: "1 January 1982",
+      type: "New referral"
     },
     {
-      name: "James",
+      name: "Pink Panther",
       benefit: "PIP",
-      nino: "1234"
+      nino: "XX XX XX XX X",
+      dob: "1 January 1991",
+      type: "No paper file"
     },
     {
-      name: "David",
+      name: "Micky Mouse",
+      benefit: "UC",
+      nino: "XX XX XX XX X",
+      dob: "1 January 1982",
+      type: "Had good cause to miss appointment"
+    },
+    {
+      name: "Bugs Bunny",
+      benefit: "UC",
+      nino: "XX XX XX XX X",
+      dob: "1 January 1982",
+      type: "New referral"
+    },
+    {
+      name: "Donald Duck",
       benefit: "PIP",
-      nino: "1234"
+      nino: "XX XX XX XX X",
+      dob: "1 January 1991",
+      type: "Further evidence returned"
     },
     {
-      name: "Paul",
-      benefit: "ESA",
-      nino: "1234"
+      name: "Charlie Brown",
+      benefit: "UC",
+      nino: "XX XX XX XX X",
+      dob: "1 January 1982",
+      type: "New evidence received"
+    },
+    {
+      name: "Yosemite Sam",
+      benefit: "UC",
+      nino: "XX XX XX XX X",
+      dob: "1 January 1982",
+      type: "No paper file"
+    },
+    {
+      name: "Woody Woodpecker",
+      benefit: "PIP",
+      nino: "XX XX XX XX X",
+      dob: "17 December 1986",
+      type: "Had good cause to miss appointment"
+    },
+    {
+      name: "Foghorn Leghorn",
+      benefit: "UC",
+      nino: "XX XX XX XX X",
+      dob: "9 February 1989",
+      type: "New referral"
+    },
+    {
+      name: "Jessica Rabbit",
+      benefit: "UC",
+      nino: "XX XX XX XX X",
+      dob: "11 October 1994",
+      type: "Further evidence returned"
     }
   ]
 
