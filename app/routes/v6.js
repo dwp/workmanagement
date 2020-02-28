c9cd26e991b6f12f99ca2788555ccada50ccb587
Phil Swan <mail@philswan.co.uk>
2020-02-28 10:25:23 +0000

    More progress on v6

--- app/routes/v6.js
+++ app/routes/v6.js
@@ -44,4 +44,41 @@ module.exports = function(router, v6Data) {
     res.render('v6/confirmation-message.html', {savedReferrals: req.session.savedReferrals, rejectedReferrals: req.session.rejectedReferrals, doneReferrals: req.session.doneReferrals});
   });
 
+
+  // Playing with saved stuff on dashboard
+  router.get('/v6/dashboard', (req, res, next) => {
+
+    // Saved
+    if (!req.session.savedReferrals){
+      req.session.savedReferrals = {
+        saved: '',
+      }
+    }
+    if (req.query.saved) {
+      req.session.savedReferrals.saved = (req.session.savedReferrals.saved + req.query.saved)
+    };
+    if (req.query.removesaved) {
+      req.session.savedReferrals.saved = req.session.savedReferrals.saved.replace(req.query.removesaved, "");
+    };
+    // Rejected
+    if (!req.session.rejectedReferrals){
+      req.session.rejectedReferrals = {
+        rejected: '',
+      }
+    }
+    if (req.query.rejected) {
+      req.session.rejectedReferrals.rejected = (req.session.rejectedReferrals.rejected + req.query.rejected)
+    };
+    // done
+    if (!req.session.doneReferrals){
+      req.session.doneReferrals = {
+        done: '',
+      }
+    }
+    if (req.query.done) {
+      req.session.doneReferrals.done = (req.session.doneReferrals.done + req.query.done)
+    };
+    res.render('v6/dashboard.html', {savedReferrals: req.session.savedReferrals, rejectedReferrals: req.session.rejectedReferrals, doneReferrals: req.session.doneReferrals});
+  });
+
 }
