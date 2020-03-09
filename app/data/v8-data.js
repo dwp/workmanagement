9d1b8b5526a54e6c5de0e7a1da34a82439c76519
Phil Swan <mail@philswan.co.uk>
2020-03-09 13:55:48 +0000

    Replace PIP and UC

--- app/data/v8-data.js
+++ app/data/v8-data.js
@@ -3,70 +3,70 @@ module.exports = function(req, res, next) {
   res.locals.cases = [
     {
       name: "Fred Flintstone",
-      benefit: "PIP",
+      benefit: "UC",
       nino: "XX XX XX XX X",
       dob: "1 January 1982",
       type: "New referral"
     },
     {
       name: "Pink Panther",
-      benefit: "PIP",
+      benefit: "UC",
       nino: "XX XX XX XX X",
       dob: "1 January 1991",
       type: "No questionnaire"
     },
     {
       name: "Mickey Mouse",
-      benefit: "PIP",
+      benefit: "UC",
       nino: "XX XX XX XX X",
       dob: "1 January 1982",
       type: "Had good cause to miss appointment"
     },
     {
       name: "Bugs Bunny",
-      benefit: "PIP",
+      benefit: "UC",
       nino: "XX XX XX XX X",
       dob: "1 January 1982",
       type: "Re-referral"
     },
     {
       name: "Donald Duck",
-      benefit: "PIP",
+      benefit: "UC",
       nino: "XX XX XX XX X",
       dob: "1 January 1991",
       type: "Further evidence returned"
     },
     {
       name: "Charlie Brown",
-      benefit: "PIP",
+      benefit: "UC",
       nino: "XX XX XX XX X",
       dob: "1 January 1982",
       type: "New evidence received"
     },
     {
       name: "Yosemite Sam",
-      benefit: "PIP",
+      benefit: "UC",
       nino: "XX XX XX XX X",
       dob: "1 January 1982",
       type: "No questionnaire"
     },
     {
       name: "Woody Woodpecker",
-      benefit: "PIP",
+      benefit: "UC",
       nino: "XX XX XX XX X",
       dob: "17 December 1986",
       type: "Had good cause to miss appointment"
     },
     {
       name: "Foghorn Leghorn",
-      benefit: "PIP",
+      benefit: "UC",
       nino: "XX XX XX XX X",
       dob: "9 February 1989",
       type: "Re-referral"
     },
     {
       name: "Lisa Simpson",
-      benefit: "PIP",
+      benefit: "UC",
       nino: "XX XX XX XX X",
       dob: "11 October 1994",
       type: "Further evidence returned"
